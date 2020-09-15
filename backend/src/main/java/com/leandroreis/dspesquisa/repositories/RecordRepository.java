2f2138751106b88cd094680230aa1ff3d30b1eb8
leandrole8 <leandro_le8@hotmail.com>
2020-09-15 19:08:37 -0300

    DTO, Service

--- backend/src/main/java/com/leandroreis/dspesquisa/repositories/RecordRepository.java
+++ backend/src/main/java/com/leandroreis/dspesquisa/repositories/RecordRepository.java
@@ -1,9 +1,10 @@
 package com.leandroreis.dspesquisa.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.leandroreis.dspesquisa.entities.Record;
 
+@Repository
 public interface RecordRepository extends JpaRepository<Record, Long> {
-
 }
